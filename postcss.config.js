cec6b1d970a6da076934ac59f4d9f1cd61b919e2
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2025-05-11 05:27:09 +0200

    🔧 Setup TailwindCSS with base config

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+export default {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  };
+// This is a PostCSS configuration file that uses Tailwind CSS and Autoprefixer as plugins.
+// It exports an object with a `plugins` property that contains the two plugins.
+// Tailwind CSS is a utility-first CSS framework that allows you to build custom designs quickly.
